ff275b0e17196119e560534be7111a5389ce6abb
Vaclav Fencl <v_fencl@utb.cz>
2024-12-26 12:24:34 +0100

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "SFML/Graphics.hpp"
+
+int main() {
+	
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Okno");
+	sf::Event e;
+
+	while (window.isOpen()) {
+		while (window.pollEvent(e)) {
+			if (e.type == sf::Event::Closed) window.close();
+		}
+	}
+	
+	
+	
+	return 0;
+}
